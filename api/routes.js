fa3f8655f47ca2a148d138abbd8f13a40fbae753
krishna-aasatech <129601901+krishna-r7@users.noreply.github.com>
2025-11-15 16:09:14 +0530

    render update

--- api/routes.js
+++ api/routes.js
@@ -1,20 +1,20 @@
-// const express = require("express");
-// const userRoutes = require("./auth/user.routes");
-// const categoryRoutes = require("./category/category.routes");
-// const expRoutes = require("./expense/exp.routes");
-// const budgetRoutes = require("./budget/budget.routes");
+const express = require("express");
+const userRoutes = require("./auth/user.routes");
+const categoryRoutes = require("./category/category.routes");
+const expenseRoutes = require("./expense/expense.routes");
+const budgetRoutes = require("./budget/budget.routes");
 
-// // const authRoutes = require("./auth/auth.routes");
+// const authRoutes = require("./auth/auth.routes");
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.use("/auth/user", userRoutes);
-// router.use("/category", categoryRoutes);
-// router.use("/expense", expRoutes);
-// router.use("/budget", budgetRoutes);
-
-
+router.use("/auth/user", userRoutes);
+router.use("/category", categoryRoutes);
+router.use("/expense", expenseRoutes);
+router.use("/budget", budgetRoutes);
 
 
 
-// module.exports = router;
+
+
+module.exports = router;
--- index.js
+++ index.js
@@ -2,15 +2,10 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const { verifyToken } = require("./middleware/authHeaders");
-// const routes = require("./api/routes");
+const routes = require("./api/routes");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-
-const userRoutes = require("./api/auth/user.routes");
-const categoryRoutes = require("./api/category/category.routes");
-const expRoutes = require("./api/expense/exp.routes");
-const budgetRoutes = require("./api/budget/budget.routes");
 
 // Middleware configurations
 app.use(express.json());
@@ -46,11 +41,7 @@ app.get("/health", (req, res) => {
 app.use(verifyToken);
 
 
-// app.use("/api", routes);
-app.use("/api/expense", expRoutes);
-app.use("/api/budget", budgetRoutes);
-app.use("/api/category", categoryRoutes);
-app.use("/api/auth", userRoutes);
+app.use("/api", routes);
 
 
 // Start
